e9dcbbfe4a42b3984f726fa8e74af0f154130d32
juicissKa <oleg.kurasov@gmail.com>
2023-10-03 10:29:36 +0300

    test

--- app/myfunc.c
+++ app/myfunc.c
@@ -7,7 +7,7 @@
 int myfunc(int b) {
     int *buffer = malloc(sizeof(char) * 1000);
     buffer [0] = b + 4;
-    // здесь должен ругаться sonarcloud, т.к. утечка памят
+    // здесь должен ругаться sonarcloud, т.к. утечка память
     return buffer[0];
 }
 
